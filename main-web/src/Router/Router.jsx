da4ac4e6afd2ec2ad11cc32b7f06258d90d9700b
shohan12727 <aishohan001@gmail.com>
2026-03-02 16:37:35 +0600

    tailwind, react router added and basic file structure added

---
+++ main-web/src/Router/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router";
+import ErrorPage from "../Components/ErrorPage";
+import Root from "../Root/Root";
+import Home from "../Pages/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <ErrorPage />,
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
